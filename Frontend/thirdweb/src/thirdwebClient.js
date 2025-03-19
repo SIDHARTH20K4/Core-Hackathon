366c32f3acccfcf4ba93b2c0606efd7df266409e
SIDHARTH20K4 <91970588+SIDHARTH20K4@users.noreply.github.com>
2025-03-19 13:08:08 +0530

    added the thirdweb implementation which maps users google account to a wallet

---
+++ Frontend/thirdweb/src/thirdwebClient.js
@@ -0,0 +1,6 @@
+// src/thirdwebClient.js
+import { createThirdwebClient } from "thirdweb";
+
+export const client = createThirdwebClient({
+    clientId: "09b90d9da477681ddcad30ee8de156fd", // Replace with your thirdweb client ID
+});
